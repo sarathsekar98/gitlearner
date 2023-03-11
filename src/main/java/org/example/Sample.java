7999c77dc9dc56b2a46da604a99461fea349adb3
sarath <sarathsekar98@gmail.com>
2023-03-11 10:09:25 +0530

    sample2 commit add here....

--- src/main/java/org/example/Sample.java
+++ src/main/java/org/example/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("welcome to you");
         System.out.println("new systerm added here.....");
+        System.out.println("sample 2 added herer");
     }
 }
